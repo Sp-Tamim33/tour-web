0cb3ddb639deb7ca5f6cd6a9ca9ecbd3c4c8f787
sp-tam1m <white.hat.tamim@gmail.com>
2022-11-03 01:47:29 +0600

    added trending tours from api

--- src/Components/TrendingTours/TrendingTours.js
+++ src/Components/TrendingTours/TrendingTours.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AllTour from '../AllTour/AllTour';
+import Loading from '../Loading/Loading';
 
 const TrendingTours = () => {
+    const [trendingTours, setTrendingTours] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://free-tour-management-api.onrender.com/tour/trending')
+            .then(res => res.json())
+            .then(data => {
+                setTrendingTours(data.data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (loading) {
+        return <div className='mx-auto'><Loading /></div>
+    }
+
+
     return (
         <div>
-            This is Trending Tours
+            <h1 className='text-center text-2xl py-4 font-semibold underline'>Let's Checkout Top Trending Tours</h1>
+            <div className='grid grid-cols-3 mx-12'>
+                {
+                    trendingTours.map(tour => <AllTour key={tour._id} tour={tour}></AllTour>)
+                }
+            </div>
         </div>
     );
 };
