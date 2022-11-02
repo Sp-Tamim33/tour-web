19819ce8e8f80e36645ff6538a8f4d88d2c6bbdb
sp-tam1m <white.hat.tamim@gmail.com>
2022-11-03 01:50:32 +0600

    added cheapest tour from api

--- src/Components/CheapestTour/CheapestTour.js
+++ src/Components/CheapestTour/CheapestTour.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AllTour from '../AllTour/AllTour';
+import Loading from '../Loading/Loading';
 
 const CheapestTour = () => {
+    const [cheapestTour, setCheapestTour] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://free-tour-management-api.onrender.com/tour/cheapest')
+            .then(res => res.json())
+            .then(data => {
+                setCheapestTour(data.data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (loading) {
+        return <div className='mx-auto'><Loading /></div>
+    }
+
+
     return (
         <div>
-            This is Cheapest Tour
+            <h1 className='text-center text-2xl py-4 font-semibold underline'>Let's Checkout Cheapest Tours</h1>
+            <div className='grid grid-cols-3 mx-12'>
+                {
+                    cheapestTour.map(tour => <AllTour key={tour._id} tour={tour}></AllTour>)
+                }
+            </div>
         </div>
     );
 };
